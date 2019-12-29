268cc5a24ba94379951a77ed94552cd49eeb573d
Aman Mittal <amandeepmittal@live.com>
2019-12-29 14:00:36 +0530

    add rn ui kitten

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components'
+import { mapping, light as lightTheme } from '@eva-design/eva'
+
+const HomeScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+)
+
+const App = () => (
+  <ApplicationProvider mapping={mapping} theme={lightTheme}>
+    <HomeScreen />
+  </ApplicationProvider>
+)
+
+export default App
